d89b390a99dcfd7c92779e6bcec5c175a302bf03
Vaishnavi <sinhavaishnavi020@gmail.com>
2026-06-07 12:51:49 +0530

    fix: datetime conversion issue in deployment

--- frontend/clean_df.py
+++ frontend/clean_df.py
@@ -3,7 +3,10 @@ import pandas as pd
 def clean_dataframe(df):
     for col in df.columns:
 
-        if df[col].dtype == object:
+        if (
+            pd.api.types.is_object_dtype(df[col])
+            or pd.api.types.is_string_dtype(df[col])
+            ):
 
             try:
                 if any(
